e4052103dffb387e14bc27ce4a04b24fe0ded13e
timcode55 <extramailboxes@gmail.com>
2023-05-22 17:38:09 -0700

    Deploy to Vercel

---
+++ utils/ratings.js
@@ -0,0 +1,7 @@
+export const ratings = [
+  { id: 1, name: "⭐️", value: 1 },
+  { id: 2, name: "⭐️⭐️", value: 2 },
+  { id: 3, name: "⭐️⭐️⭐️", value: 3 },
+  { id: 4, name: "⭐️⭐️⭐️⭐️", value: 4 },
+  { id: 5, name: "⭐️⭐️⭐️⭐️⭐️", value: 5 },
+];
